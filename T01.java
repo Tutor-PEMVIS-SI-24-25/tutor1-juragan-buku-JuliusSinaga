955f2fe0ca2554f276d6a6f96b6a83fe5f2cfb16
Julius Sinaga <143679205+JuliusSinaga@users.noreply.github.com>
2024-09-04 21:27:29 +0700

    Finish

--- T01.java
+++ T01.java
@@ -1,3 +1,16 @@
-// NIM - NAMA
-// NIM - NAMA
+// 12S23030 - Simorangkir Jonathan
+// 12S23016 - Frank ganteng
 
+import java.util.*;
+import java.lang.Math;
+
+class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int bilanganbulat;
+
+        bilanganbulat = input.nextInt();
+        System.out.println(bilanganbulat);
+    }
+}
